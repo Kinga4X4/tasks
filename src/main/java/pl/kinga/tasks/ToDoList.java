7d9029cd57c55b6217797080e4efc20472ede52a
Kinga Paszkiewicz <kingaa.kulig@gmail.com>
2021-04-26 23:13:38 +0200

    Utworzenie projektu

--- src/main/java/pl/kinga/tasks/ToDoList.java
+++ src/main/java/pl/kinga/tasks/ToDoList.java
@@ -15,4 +15,49 @@ public class ToDoList {
     private boolean done;
     private LocalDate deadline;
 
+    ToDoList() {}
+
+    public ToDoList(long id, String taskName, boolean done, LocalDate deadline) {
+        this.id = id;
+        this.taskName = taskName;
+        this.done = done;
+        this.deadline = deadline;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + taskName + " " + done + " " + deadline;
+    }
 }
